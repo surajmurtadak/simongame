0719fe21300b656145867c04f2542da22b6fb2a1
surajmurtadak <surajmurtadak@gmail.com>
2022-11-08 10:38:44 +0530

    working fine

--- game.js
+++ game.js
@@ -4,10 +4,45 @@ let userClick = [];
 let pattern = [];
 let started = false;
 
+$(document).keypress(function () {
+  if (!started) {
+    nextstep();
+    started = true;
+  }
+});
+
 $(".btn").click(function () {
   let click = $(this).attr("id");
+  userClick.push(click);
   btnpress(click);
+  play(click);
+  checkAnswer(userClick.length - 1);
 });
+
+//for nextStep
+function nextstep() {
+  level++;
+  $("h1").text(`level ${level}`);
+  userClick = [];
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomColor = colors[randomNumber];
+  pattern.push(randomColor);
+  setTimeout(function () {
+    btnpress(randomColor);
+    play(randomColor);
+  }, 700);
+}
+
+//check answer
+function checkAnswer(check) {
+  if (userClick[check] === pattern[check]) {
+    if (userClick.length === pattern.length) {
+      nextstep();
+    }
+  } else {
+    gameOver();
+  }
+}
 
 //button press animation
 function btnpress(clk) {
@@ -15,4 +50,23 @@ function btnpress(clk) {
   setTimeout(function () {
     $(`.${clk}`).removeClass("pressed");
   }, 200);
+}
+
+//sound play
+function play(snd) {
+  let sound = new Audio(`/sounds/${snd}.mp3`);
+  sound.play();
+}
+
+//gameOver
+function gameOver() {
+  level = 0;
+  pattern = [];
+  userClick = [];
+  $("h1").text("Game Over, press any key to start again");
+  $("body").addClass("game-over");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 400);
+  play("wrong");
 }
